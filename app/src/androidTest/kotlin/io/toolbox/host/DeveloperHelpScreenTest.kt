474fde1e81bf7f43d35415e67b3786169206a3bd
gkeyes <25716974+gkeyes@users.noreply.github.com>
2026-09-03 11:04:38 +0800

    fix: use Compose node member assertion in help test

--- app/src/androidTest/kotlin/io/toolbox/host/DeveloperHelpScreenTest.kt
+++ app/src/androidTest/kotlin/io/toolbox/host/DeveloperHelpScreenTest.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasTestTag
