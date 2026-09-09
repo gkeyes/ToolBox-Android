d43e7f4a14ae5fa84cf7da3ca6f9ac9f27ffa6e8
DoraC <25716974+gkeyes@users.noreply.github.com>
2026-09-09 21:01:58 +0800

    test: remove invalid import for SemanticsNodeInteraction member assertion

--- app/src/androidTest/kotlin/io/toolbox/host/ui/VisibleUiStateTest.kt
+++ app/src/androidTest/kotlin/io/toolbox/host/ui/VisibleUiStateTest.kt
@@ -3,7 +3,6 @@ package io.toolbox.host.ui
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import kotlinx.coroutines.flow.MutableStateFlow
